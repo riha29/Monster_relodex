0503e1c3189201aae0460ae884194b7e4162b9c8
Faozia Islam <faozia.islam.29@gmail.com>
2022-09-16 02:00:40 +0600

    fetching and showing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import {component} from 'react'
+import {Component} from 'react'
 
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructot() {
+  constructor() {
     super(); 
 
     this.state = {
-      name: { firstName: 'Faozia', lastName: 'Islam'}
-      company: 'omega',
+      monster: [],
     };
   }
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => this.setState(() => {
+      return {monster: users}
+    },
+    () => {
+      console.log(this.state)
+    }
+    ))
+  }
+
   render() {
-    return <div className= 'App'></div>
+    return (
+      <div className= 'App'>
+        <input className='search-bos' type='search' placeholder='search monsters' onClick={(event) => {
+          console.log(event.target.value)
+        }}/>
+        {
+          this.state.monster.map((monster) => {
+          return <div key={monster.id}>
+            <h1>
+              {monster.name}
+            </h1>
+          </div>
+          }
+         )
+        }
+    </div>
+    )
   }
 }
-
-
 
 export default App;
