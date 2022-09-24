4411abb7f1027b740877420d1045daabcd90cf7d
Faozia Islam <faozia.islam.29@gmail.com>
2022-09-24 20:35:48 +0600

    error found

--- src/component/card-list/card-list.component.jsx
+++ src/component/card-list/card-list.component.jsx
@@ -1,21 +1,34 @@
 import {Component} from 'react'
 
+import './card-list.styles.css'
+
 class CardList extends Component {
     
     render () {
-        console.log('render from cardlist')
-        const {monster}= this.props
+
+        const {monster} = this.props
+
         return (
-            <div className='card-list'>
-                {monster.map(monster => (
-                    <h1 key= {monster.id}>
-                        {monster.name}
-                    </h1>
-                ))}
+            <div className='card-list' key={monster.id}>
+                {/* <img alt= {`monster ${name}`} src= {`https://robobash.org/${id}?set=set2&size=180x180`} />
+                <h2>
+                    {name}
+                </h2>
+                <p>
+                    {email}
+                </p> */}
+                {monster.map((monster) => {
+                    const {name, email, id}= monster 
+                    return (
+                        <div className='card-container' key= {id}>
+                            <img alt={`monster ${name}`} src= {`https://robohash.org/ ${id}?set=set2`} />
+                            <h2> {name} </h2>
+                            <p>{email} </p>
+                    </div>
+                    )})}
             </div>
         )
     }
 }
-
 
 export default CardList
