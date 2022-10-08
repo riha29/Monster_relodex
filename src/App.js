82184b8c124039c22b411a33aa4a18ba0e14198e
Faozia Islam <faozia.islam.29@gmail.com>
2022-10-08 11:25:20 +0600

    use effect using

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import {Component} from 'react'
+
+import {Component, useEffect} from 'react'
 import { useState } from 'react';
 
 import CardList from './component/card-list/card-list.component';
@@ -8,30 +9,51 @@ import './App.css';
 const App = () => {
   const [searchField, setSearchField] = useState('')
   const [monster, setMonster]= useState([])
+  const [filterMonster, setFilterMonster]= useState(monster)
+  const [stringField, setStringField]= useState('')
 
   console.log('render')
 
-  fetch('https://jsonplaceholder.typicode.com/users')
+  useEffect( () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
     .then((response) => response.json())
     .then((users) => setMonster(users)
     )
+  }, [])
 
+  useEffect( () => {
+    const newFilteredMonster= monster.filter( (monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField)
+    })
+    setFilterMonster(newFilteredMonster)
+
+    console.log('effect is firing')
+  }, [monster, searchField])
+
+  
   const onSearchChange= (event) => {
       const searchFieldString = event.target.value.toLocaleLowerCase()
       setSearchField(searchFieldString)
   }
 
+  const onStringChange = (event) => {
+    setStringField(event.target.value)
+  }
+
   const filteredMonster= monster.filter((monsters)=> {
     return monsters.name.toLocaleLowerCase().includes(searchField)
-    })
+     })
+
+  console.log(filteredMonster)
 
   
   return (
     <div className= 'App'> 
     <h1 className='app-title'>
-      Monster Roledex Cards
+      Monster Roledex
     </h1>
     <SearchBox onChangeHandler= { onSearchChange } placeholder= 'search monsters' className= 'monster-search-box' />
+    <SearchBox onChangeHandler= { onStringChange } placeholder= 'search string'/>
     <CardList monster= {filteredMonster} />
     </div>
   )
